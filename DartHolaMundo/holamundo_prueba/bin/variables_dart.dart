61133b50ce3f18f9e32cac1da3b525a1e35584a6
Stuart_lil <djjonhy@outlook.com>
2023-06-26 23:19:27 -0600

    pruebas para variables_dart

---
+++ DartHolaMundo/holamundo_prueba/bin/variables_dart.dart
@@ -0,0 +1,12 @@
+void main(){
+  var nombre = "Luis";
+  var edad = 29 ;
+
+  print("probando tu nombre: " + nombre);
+  print(edad);
+  //VER TIPOS DE VARIABLES
+  print("la variable edad es un tipo de dato:"  );
+  print(edad.runtimeType); 
+  print("la variable nombre es un tipo de dato:"  );
+  print(nombre.runtimeType);
+}
