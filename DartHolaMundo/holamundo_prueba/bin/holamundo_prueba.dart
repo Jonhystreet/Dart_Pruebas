3aeb736343928ab7e27c08fe3907b53b623a8567
Stuart_lil <djjonhy@outlook.com>
2023-06-26 22:54:05 -0600

    Agregado de nuevas

---
+++ DartHolaMundo/holamundo_prueba/bin/holamundo_prueba.dart
@@ -0,0 +1,5 @@
+import 'package:holamundo_prueba/holamundo_prueba.dart' as holamundo_prueba;
+
+void main(List<String> arguments) {
+  print('Hello world: ${holamundo_prueba.calculate()}!');
+}
---
+++ DartHolaMundo/holamundo_prueba/test/holamundo_prueba_test.dart
@@ -0,0 +1,8 @@
+import 'package:holamundo_prueba/holamundo_prueba.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
