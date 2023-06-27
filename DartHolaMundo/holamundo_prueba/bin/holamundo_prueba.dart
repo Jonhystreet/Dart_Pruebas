d05ae7ac6f0959865e9e0acf5eb7d132a8c6d2cb
Stuart_lil <djjonhy@outlook.com>
2023-06-26 22:56:37 -0600

    probando rama

--- DartHolaMundo/holamundo_prueba/bin/holamundo_prueba.dart
+++ DartHolaMundo/holamundo_prueba/bin/holamundo_prueba.dart
@@ -2,4 +2,5 @@ import 'package:holamundo_prueba/holamundo_prueba.dart' as holamundo_prueba;
 
 void main(List<String> arguments) {
   print("Hola mundo que tal estas?");
+  print("como te encuentras");
 }
