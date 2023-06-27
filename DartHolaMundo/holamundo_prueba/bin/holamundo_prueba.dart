593a3d965608fa2593c5f119933d7bc6909dadbc
Stuart_lil <djjonhy@outlook.com>
2023-06-26 23:10:52 -0600

    Probando un print

--- DartHolaMundo/holamundo_prueba/bin/holamundo_prueba.dart
+++ DartHolaMundo/holamundo_prueba/bin/holamundo_prueba.dart
@@ -1,6 +1,3 @@
-import 'package:holamundo_prueba/holamundo_prueba.dart' as holamundo_prueba;
-
-void main(List<String> arguments) {
-  print("Hola mundo que tal estas?");
-  print("como te encuentras");
+void main(){
+  print("HOla mundo como estas?");
 }
--- DartHolaMundo/holamundo_prueba/test/holamundo_prueba_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:holamundo_prueba/holamundo_prueba.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
